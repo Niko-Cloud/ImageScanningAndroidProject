0244d55ed62bf2e3dd78365df39a752d1f56e9d4
Niko-Cloud <indraniko20@gmail.com>
2023-12-03 21:12:34 +0700

    add camera x

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,9 @@ dependencies {
 
     //Circle Image
     implementation("de.hdodenhof:circleimageview:3.1.0")
+
+    //Camera X
+    implementation("androidx.camera:camera-camera2:1.3.0")
+    implementation("androidx.camera:camera-lifecycle:1.3.0")
+    implementation("androidx.camera:camera-view:1.3.0")
 }
--- app/src/main/java/com/ch2ps126/capstoneproject/ui/MainActivity.kt
+++ app/src/main/java/com/ch2ps126/capstoneproject/ui/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.ch2ps126.capstoneproject.ui
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.MenuProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,6 +18,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.ch2ps126.capstoneproject.R
 import com.ch2ps126.capstoneproject.databinding.ActivityMainBinding
+import com.ch2ps126.capstoneproject.ui.camera.CameraActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
 
@@ -19,10 +26,31 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Toast.makeText(this, "Permission request granted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Permission request denied", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    private fun allPermissionsGranted() =
+        ContextCompat.checkSelfPermission(
+            this,
+            REQUIRED_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (!allPermissionsGranted()) {
+            requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+        }
 
         val bottomNavigationView: BottomNavigationView = binding.bottomNavigationView
         bottomNavigationView.background = null
@@ -39,5 +67,13 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController)
 
+        binding.fab.setOnClickListener {
+            intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    companion object{
+        private const val REQUIRED_PERMISSION = Manifest.permission.CAMERA
     }
 }
